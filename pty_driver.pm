1072ade8a2d50730f720fbaf9b8472f076a0bd4c
Joe Schaefer <joe@sunstarsys.com>
2020-04-18 12:47:04 -0400

    remarks

--- pty_driver.pm
+++ pty_driver.pm
@@ -97,10 +97,9 @@ END { defined $mterm and ReadMode restore => $mterm; sleep 1; }
 
 =head2 HELPER FUNCTIONS
 
-=item echo_enabled ($)
+=item echo_enabled
 
-Takes an fd as argument, typically MASTER_PTY_FD or SLAVE_PTY_FD.
-Returns true if the terminal attached to that fd has echo enabled.
+Returns true if the slave terminal has echo enabled.
 
 =cut
 
