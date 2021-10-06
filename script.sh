72669c0e7c6fd2fec8bf37b82b6cf4cc4b539da0
Joe Schaefer <joseph.schaefer@blackstone.com>
2021-10-06 09:47:53 -0400

    linting

--- script.sh
+++ script.sh
@@ -5,15 +5,15 @@ append=0
 quiet=0
 
 while getopts aq o; do
-    case "$o" in
-        a)append=1;;
-        q)quiet=1;;
-        ?)exit 1;;
-    esac
+  case "$o" in
+    a) append=1 ;;
+    q) quiet=1 ;;
+    ?) exit 1 ;;
+  esac
 done
 
 while [ ! "$1" = "$(echo "$1" | sed -e 's/^-//')" ]; do
-    shift
+  shift
 done
 
 file=${1-typescript}
@@ -25,18 +25,18 @@ if [ $quiet -eq 0 ]; then
   echo Script started, file is $file
 fi
 if [ $append -eq 1 ]; then
-  echo Script started on `date` >> $file
+  echo Script started on $(date) >>$file
 else
-  echo Script started on `date` > $file
+  echo Script started on $(date) >$file
 fi
 
-echo command: $cmd "$@" >> $file
+echo command: $cmd "$@" >>$file
 
 SCRIPT=$file pty -- $cmd "$@" | tee -a $file
 
-echo >> $file
-echo Script done on `date` >> $file
+echo >>$file
+echo "Script done on $(date)" >>$file
 if [ $quiet -eq 0 ]; then
-    echo Script done, file is $file
+  echo Script done, file is $file
 fi
 echo
